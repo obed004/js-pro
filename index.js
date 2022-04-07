909288099b8460990d04a3eb09ab993859b7ecda
obed004 <100470457+obed004@users.noreply.github.com>
2022-04-07 12:15:43 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-/*var b = 50;
-console.log(b);
-document.writeln(b)
-document.getElementById("hello").innerHTML = "hello world";
-for (var i = 10; i<=50; i+=2){
-    document.writeln(i)
-    
-};*/
-
-
-//sum of variables
-const  sum=[10 ,12, 14,16 ,18 ,20 ,22 ,24 ,26 ,28 ,30 ,32 ,34 ,36 ,38 ,40 ,42 ,44 ,46 ,48 ,50].reduce(function(result,item){
-    return result+item;},0)
-    document.writeln(sum);       
+const array =["car", "house","phones"];
+const hasValue= array.includes("house");
+//condition
+if(hasValue){
+    console.log("array contains value.");
+}
+else{
+    console.log("does not contain value");
+}
+//console.log(array);
